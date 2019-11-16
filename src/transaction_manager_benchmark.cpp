6ec62aa5971391b22164ab33cbc1f5b2ad3cedc5
Kulpreet Singh <zapfmann@gmail.com>
2019-11-16 13:53:29 +0100

    Add google benchmark as dependency.
    Make sure benchmark sample code works.

--- src/transaction_manager_benchmark.cpp
+++ src/transaction_manager_benchmark.cpp
@@ -18,10 +18,23 @@
  */
 #include <iostream>
 
-#include "benchmark.hpp"
+#include <benchmark/benchmark.h>
 
-int main()
-{
-    std::cout << "hello world!" << std::endl;
+#include "database_benchmark.hpp"
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
 }
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
 
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+BENCHMARK_MAIN();
